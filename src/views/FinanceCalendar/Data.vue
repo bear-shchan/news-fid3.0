8ed2e7670d239ecdc19d07ec5f7b75fb4037d515
bear-shchan <384595586@qq.com>
2017-05-09 17:30:33 +0800

    [view] 财经日历-数据done

--- src/views/FinanceCalendar/Data.vue
+++ src/views/FinanceCalendar/Data.vue
@@ -7,18 +7,21 @@
       infinite-scroll-immediate-check="false">
       <div class="list-item layout-box"
         v-for="(item, index) in list"
-        @click="gotoDetail(item.id)"
         >
+        <!-- @click="gotoDetail(item.id)" -->
           <div class="date" v-if="item.show_date_b">
-            {{ item.show_date }}
+            {{ item.showDate }}
           </div>
           <div class="date" v-else>
           </div>
           <ul class="text box-col">
             <li>
-              <p class="tt">
-                {{ item.events }}
-                <a class="concept" v-if="item.concept">{{ item.concept }}</a>
+              <p class="title">
+                {{ item.country }}
+                <span class="fr">{{item.showTime}}</span>
+              </p>
+              <p class="desc">
+                {{ item.item }}
               </p>
             </li>
           </ul>
@@ -29,7 +32,7 @@
 
 <script>
 import router from '../../router'
-import moment from 'moment'
+// import moment from 'moment'
 
 import { mapActions } from 'vuex'
 
@@ -46,17 +49,13 @@ export default {
     this.getData()
   },
   methods: {
-    ...mapActions(['SET_EVENT_ITEM']),
+    ...mapActions(['SET_CALENDAR_ITEM']),
     getData () {
       this.listBusy = true
-      let dateBegin = moment().format('YYYY-MM-DD') + ' 00:00:00'
-      this.$http.get('/fidnews/v1/calendarApp/event', {
+      this.$http.get('/fidnews/v1/calendar/economic_datas', {
         params: {
-          orderSeq: 'asc',
-          orderCol: 'released_date',
-          number: 15,
-          pageNo: this.pageNo,
-          date_begin: dateBegin
+          size: 15,
+          pageNo: this.pageNo
         }
       })
       .then((data) => {
@@ -67,15 +66,15 @@ export default {
         var beforeTime = ''
         for (; i < len; i++) {
           // 处理数据
-          list[i].show_date = (list[i].show_date).split(' ')[0].split('-')[1] + '-' + (list[i].show_date).split(' ')[0].split('-')[2]
-          list[i].concept = list[i].concept.split('•')[1]
-          if (beforeTime === list[i].show_date) {
+          list[i].showDate = (list[i].show_date).split(' ')[0].split('-')[1] + '-' + (list[i].show_date).split(' ')[0].split('-')[2]
+          if (beforeTime === list[i].showDate) {
             list[i].show_date_b = false
           } else {
             list[i].show_date_b = true
           }
-          beforeTime = list[i].show_date
-          // console.log(list[i].show_date_b)
+          beforeTime = list[i].showDate
+          //
+          list[i].showTime = (list[i].show_date).split(' ')[1].substr(0, 5)
         }
         if (this.pageNo !== 1) {
           var j = 0
@@ -95,8 +94,11 @@ export default {
       var len = this.list.length
       for (; i < len; i++) {
         if (vm.list[i].id === id) {
-          vm.SET_EVENT_ITEM(vm.list[i])
-          router.push('eventDetail')
+          vm.list[i].description_paraphrase = vm.list[i].description.split('： ')[1].split('主指标关注原因')
+          vm.list[i].description_reason = vm.list[i].description.split('： ')[2]
+          console.log(vm.list[i].description_reason)
+          vm.SET_CALENDAR_ITEM(vm.list[i])
+          router.push('calendarDetail')
           return
         }
       }
@@ -110,6 +112,7 @@ export default {
   position: relative;
   padding-top: 0.53rem;
   font-size: 16px;
+  color: #2e2e37;
 }
 .list-item .date{
   text-indent: 0.4rem;
@@ -119,16 +122,14 @@ export default {
   border-bottom: 1px solid #ececec;
   padding-bottom: 0.53rem;
 }
-.list-item .tt{
-  width: 72%;
+
+.title{
+  padding-right: 0.4rem;
+  font-size: 12px;
+  color: #66667a;
 }
-.concept{
-  position: absolute;
-  right: 0.4rem;
-  top: 0.67rem;
-  padding: 0 0.13rem;
-  font-size: 10px;
-  background-color: #f4ce46;
-  line-height: 18px;
+.desc{
+  width: 90%;
+  line-height: 23px;
 }
 </style>
