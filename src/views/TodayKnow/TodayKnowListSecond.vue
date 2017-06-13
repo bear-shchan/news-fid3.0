bb547682701c2560413ad9867ba0e6076dfece38
Angie <864814910@qq.com>
2017-06-13 11:09:48 +0800

    今日必知增加上拉加载

--- src/views/TodayKnow/TodayKnowListSecond.vue
+++ src/views/TodayKnow/TodayKnowListSecond.vue
@@ -1,21 +1,33 @@
 <template>
   <div>
-    <imgs-list :list-arr="listArr" path="/todayKnowDetail/"></imgs-list>
+    <imgs-list :list-arr="list" path="/todayKnowDetail/"></imgs-list>
+    <loadmore
+      v-on:getData="getList"
+      :loading="loading"
+      :showLoading="!firstRequest"
+      :done="done">
+    </loadmore>
   </div>
 </template>
 
 <script>
 import contrastDate from '@/assets/js/contrastDate.js'
 import ImgsList from '@/components/ImgsList.vue'
-
+import Loadmore from '@/components/Loadmore.vue'
 export default {
   name: '',
   components: {
-    ImgsList
+    ImgsList,
+    Loadmore
   },
   data () {
     return {
-      listArr: []
+      list: [],
+      publishTime: '',
+      firstRequest: true,
+      doneText: '没有更多数据',
+      loading: false,
+      done: false
     }
   },
   created () {
@@ -23,46 +35,36 @@ export default {
   },
   methods: {
     getList () {
+      this.loading = true
       this.$http.get('/fidnews/v1/geek/v3/queryFocusTodayListByPro', {
         params: {
           user: 'fidinner',
           key: 'ab54eae187cd5cf4e89fed7a4e62586e',
-          publishTime: '',
+          publishTime: this.publishTime,
           proId: this.$route.params.tagname,
-          size: ''
+          size: 10
         }
       })
       .then((res) => {
         let data = res.data
+        if (data.length === 0) {
+          this.done = true
+          return
+        }
         for (let i = 0, len = data.length; i < len; i++) {
           data[i].date = contrastDate(data[i].publishTime)
           data[i].pictureUrl = data[i].pictureUrl.split(';')
           data[i].pictureNum = data[i].pictureUrl.length
         }
-        this.listArr = data
+        if (this.firstRequest) {
+          this.firstRequest = false
+        }
+        this.$set(this, 'list', this.list.concat(data))
+        this.publishTime = this.list[this.list.length - 1].publishTime
+        this.loading = false
         console.log(data)
       })
     }
-    // getList () {
-    //   this.$http.get('/fidnews/v1/geek/v2/queryFocusTodayList', {
-    //     params: {
-    //       user: 'geek',
-    //       key: '4c039f2967c4d93e9674ffb037724187',
-    //       publishTime: '',
-    //       size: 10
-    //     }
-    //   })
-    //   .then((res) => {
-    //     let data = res.data
-    //     for (let i = 0, len = data.length; i < len; i++) {
-    //       data[i].date = contrastDate(data[i].publishTime)
-    //       data[i].pictureUrl = data[i].pictureUrl.split(';')
-    //       data[i].pictureNum = data[i].pictureUrl.length
-    //     }
-    //     this.listArr = data
-    //     console.log(data)
-    //   })
-    // }
   }
 }
 </script>
